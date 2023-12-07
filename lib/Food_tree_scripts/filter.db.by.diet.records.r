f92a96a239f34f0a8c2083fc6114a36225ee64b0
Rie Sadohara <56049845+RieSadohara@users.noreply.github.com>
2023-12-07 14:42:50 +0900

    delete docs and specify root as source

--- lib/Food_tree_scripts/filter.db.by.diet.records.r
+++
@@ -1,9 +0,0 @@
-# filters the entire database of foods by only those that were actually reported in this dataset
-# useful for creating trees limited only to foods collected
-FilterDbByDietRecords <- function(food_database_fn, food_records_fn, output_fn)
-{
-    fdata <- read.table(food_database_fn, header = TRUE, sep="\t", colClasses="character", quote="", strip.white=T)
-    diet <- read.table(food_records_fn, header = TRUE, sep="\t", colClasses="character", quote="", strip.white=T)
-    valid_ids <- intersect(fdata$FoodID, diet$FoodID)
-    write.table(fdata[fdata$FoodID %in% valid_ids,], output_fn, sep="\t", quote=FALSE, row.names=FALSE)
-}
